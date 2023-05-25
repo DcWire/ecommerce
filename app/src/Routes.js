e884ad98d9b89b781d86ce33e17fd1af08426b27
Anas <anasthegreat986@gmail.com>
2023-05-26 01:03:44 +0530

    Design home page

--- app/src/Routes.js
+++ app/src/Routes.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {BrowserRouter, Route, Routes as Switch} from 'react-router-dom';
 import Home from './core/Home'
-import AdCreate from './Ad/CreateAd'
+// import AdCreate from './Ad/CreateAd'
 
-import User from './core/User'
+// import User from './core/User'
 console.log(Home);
 
 const Routes = () => {
@@ -11,11 +11,7 @@ const Routes = () => {
         <BrowserRouter>
             <Switch>
                 <Route exact path="/" element={<Home />} />
-                <Route exact path="/ad/:id" element={<Ad />} />
-                {/* <Route exact path="/ad/create" element={<Ad />} />
-                <Route exact path="/user/:id" element={<Ad />} />
-                <Route exact path="/user/create" element={<Ad />} /> */}
-                <Route path="*" element={<h1>Not found</h1>} />
+                
             </Switch>
         </BrowserRouter>
     );
--- app/src/core/Home.js
+++ app/src/core/Home.js
@@ -1,7 +1,8 @@
 import React, {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom'
 import {getAds} from './Ad'
-
+import Navbar from './Navbar'
+import './Home.css'
 
 function Home() {
   const [ads, setAds] = useState([]);
@@ -24,26 +25,59 @@ function Home() {
         loadAds();
     }, []);
 
+    const styles = {
+        margin: '20px',
+        marginBottom: '50px',
+        // border: '1px solid black',
+        // borderRadius: '10px',
+    }
     return (
+         
         <div>
-            <div>
-                Ads 
+            <Navbar />
+            <div className="jumbotron">
+                <h1 className="display-4">
+                    Arsheyan Saala
+                </h1>
+
+                <hr className="my-4" />
+
+                <p className="lead">
+                    This is a simple ecommerce website where you can create, update, delete and view ads.
+                </p>
+
+            </div>
+            <div className="container ml-2">
+                <h1>
+                    New arrivals
+                </h1>
             </div>
             <div>
                 {
-                    ads.map((ad, i) => 
-                        { 
-                            return <Link to={`/ad/${ad._id}`}>
-                            <div key={ad._id} style={{margin: '20px', border: '2px solid black', width: '25%', textAlign: 'center'}} > 
-                                <div>{ad.title}</div>
-                                <div>{ad.description}</div>
-                                <div>{ad.price}</div>
-                                <div>{ad.category}</div>
-                                <div>{ad.postedBy}</div>
-                            </div> 
+
+                    <div className="container">
+                    <div class="row">
+                    {ads.map((ad) => (
+                        <div key={ad._id} className="col-md-3" style={styles}>
+                        <div clasName="container" >
+                            <Link to={`/ad/${ad._id}`} style={{textDecoration: 'none'}}>
+                                <img className="card-img-top" src="https://picsum.photos/200" />
+                                <div className="container" style={{border:"1px solid black", borderBottomRightRadius:"5%", borderBottomLeftRadius:"5%"}}>
+                                    <div className="card-body" style={{marginLeft: "-5px"}}>
+                                        <h5 className="card-title">{ad.title}</h5>
+                                    </div>
+                                    <ul className="list-group list-group-flush" style={{marginLeft: "-5px"}}>
+                                        <li className="list-group-item">Category: {ad.category}</li>
+                                        <li className="list-group-item">Price: {ad.price}</li>
+                                        <li className="list-group-item">Location: {ad.location}</li>
+                                    </ul>
+                                </div>
                             </Link>
-                        }
-                    )
+                        </div>
+                        </div>
+                    ))}
+                    </div>
+                    </div>
                 }
 
             </div>
